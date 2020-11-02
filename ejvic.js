fc6220c72c2e9dbac0d12d5c19fec56adbef1c3b
CBorj <71759121+CBorj@users.noreply.github.com>
2020-11-02 18:43:05 +0100

    Add files via upload

---
+++ ejvic.js
@@ -0,0 +1,7 @@
+const sup=["Remo en trx","Remo con goma","Hombro con peso","Flexiones en suelo","Flexiones en pelota","Flexiones en trx","Hombro con goma en el culo","Sentadilla con hombro","Triceps","Triceps pisando goma","Biceps pisando goma"];
+var Sa,Sb,Sc,Sd,l;
+l=sup.length;
+Sa=parseInt(Math.random()*l);
+do{Sb=parseInt(Math.random()*4);}while(Sa==Sb);
+do{Sc=parseInt(Math.random()*4);}while(Sa==Sc || Sb==Sc);
+do{Sd=parseInt(Math.random()*4);}while(Sa==Sd || Sb==Sd || Sc==Sd);
